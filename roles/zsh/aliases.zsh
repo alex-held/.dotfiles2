7abeb22eed510a00003e4dcbbe7b81e42d58d6d8
alex-held <git@alexheld.io>
2020-12-29 09:00:07 +0100

    refactor(zsh): add pubkey alias

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
-
 alias ls='exa -b --links --long -a --git'
 alias l='exa -@ --git  -H -g -a --group-directories-first --long --modified'
 alias l='exa -a -F -@  --git --git-ignore --group-directories-first  --long  -t modified'
@@ -16,3 +15,8 @@ alias cdd='cd ~/source/repos'
 # GIT
 alias gs='git status'
 alias gl='glog'
+
+
+# Public Key to Clipboard
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
